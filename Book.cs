95fee25e125643d31050cef0ae20fc46a7d7489e
antonyessac351-debug <antonyessac@gmail.com>
2026-07-30 14:05:16 +0300

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Session_5
+{
+    internal class Book
+    {
+        private string password = "secret";
+        internal int copiesInStock = 5;
+         public string Title;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Session_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Book book = new Book();
+
+
+            #region // Question 
+
+            //book.pass                  --->  // not found because it private Field
+
+            //Console.WriteLine(book);
+
+            #endregion
+
+
+
+
+        }
+    }
+}
